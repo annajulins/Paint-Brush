f4a02b981fd9f5d67445d92db6576af65ac48ec0
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:05:19 -0300

    Add comments to Oval class methods
    Added comments to clarify coordinate storage and size check.

--- oval.py
+++ oval.py
@@ -1,11 +1,11 @@
 class Oval(Figura):
     def __init__(self, x1, y1, x2, y2, cor, bg):
         super().__init__(cor, bg)
-        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #armazena as coordenadas dos "cantos"
 
     def desenhar(self, canvas):
         canvas.create_oval(self.x1, self.y1, self.x2, self.y2, 
                            outline = self.cor, fill = self.bg)
 
     def incompleta(self):
-        return (self.x1, self.y1) == (self.x2, self.y2)
+        return (self.x1, self.y1) == (self.x2, self.y2) #se os dois cantos forem iguais, o oval nao tem tamanho
