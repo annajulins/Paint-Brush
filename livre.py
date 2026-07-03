cb3400fe0d312532ce89588ec00d646a3b822bcd
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:10:18 -0300

    Update livre.py

--- livre.py
+++ livre.py
@@ -1,14 +1,14 @@
 class Livre(Figura):
     def __init__(self, cor):
         super().__init__(cor, "")
-        self.pontos = []
+        self.pontos = [] #é criada uma lista para armazenar os pontos do rabisco
 
     def adicionar_ponto(self, x, y):
-        self.pontos.append((self.x, self.y))
+        self.pontos.append((x, y)) #a cada passada de mouse, as coordenadas registradas sao armazenadas na lista
 
     def desenhar (self, canvas):
-        if len(self.pontos) > 1:
+        if len(self.pontos) > 1: #o tkinter interliga todos os pontos da lista
             canvas.create_line(self.pontos, fill = self.cor)
 
-    def incompleta(self):
+    def incompleta(self): #se tem apenas um ponto, nao ha linha
         return len(self.pontos) <= 1
