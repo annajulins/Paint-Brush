b37f2d5f0c74dff5ad73ad637a0eef52b9482c70
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:00:44 -0300

    Update linha.py

--- linha.py
+++ linha.py
@@ -1,7 +1,7 @@
 class Linha(Figura):
-    def __init__(self, x1, y1, x2, y2, cor): #inicialmente registra as quatro coordenadas como dois pares repetidos
+    def __init__(self, x1, y1, x2, y2, cor):
         super().__init__(cor, "") #a linha não tem bg, por isso o ""
-        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #as quatro coordenadas, equivalentes ao ponto inicia e ao final, são armazenadas
 
     def desenhar(self, canvas): #no caso de arrasto do mouse, a linha é criada a partir do ponto inicial e do ponto final, sendo a sua cor definida por "fill"
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, 
