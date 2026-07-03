d5590df5e97f4ecbf4cd06d0318aa4251e3b3b29
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:06:37 -0300

    Update retangulo.py

--- retangulo.py
+++ retangulo.py
@@ -1,11 +1,11 @@
 class Retangulo(Figura):
     def __init__(self, x1, y1, x2, y2, cor, bg):
-        super()._init__(cor, bg)
-        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+        super().__init__(cor, bg)
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #cantos armazenados por meio de dois pares de coordenadas
 
     def desenhar(self, canvas):
         canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, 
                                 outline = self.cor, fill = self.bg)
 
     def incompleta(self):
-        return (self.x1, self.y1) == (self.x2, self.y2)
+        return (self.x1, self.y1) == (self.x2, self.y2) #raciocinio parece com o de oval, ja que, na igualdade dos cantos, o retangulo nao tem tamanho
