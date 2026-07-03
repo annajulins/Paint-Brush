3e6698f55867182aea517e8b5bf0861c5a0bda79
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 13:57:18 -0300

    Implement shape drawing events in geral.py
    Add event handling for starting and including shapes.

--- geral.py
+++ geral.py
@@ -10,3 +10,24 @@ class Geral:
         self.tipo = StringVar() #variaveis do tkinter
         self.cor = StringVar(value = self.cores[0]) #cor da borda
         self.bg = StringVar(value = self.cores[0]) #cor do preenchimento
+
+#eventos
+    def iniciar(self, event): #chamada quando o usuario pressiona o mouse
+        tipo = self.tipo.get() #obtem o tipo escolhido de imagem pelo usuario
+
+        if tipo == 'Linha':
+            self.figura_nova = Linha(event.x, event.y, event.x, event.y, self.cor.get()) #inicialmente começo e fim são iguais, por isso o ponto fica "duplicado"
+        elif tipo == 'Circulo':
+            self.figura_nova = Circulo(event.x, event.y, 0, self.cor.get(), self.bg.get()) #raio comeca de tamanho nulo
+        elif tipo == 'Oval':
+            self.figura_nova = Oval(event.x, event.y, event.x, event.y, self.cor.get(), self.bg.get()) #os cantos sao os mesmos 
+        elif tipo == 'Retangulo':
+            self.figura_nova = Retangulo(event.x, event.y, event.x, event.y, self.cor.get(), self.bg.get()) #os cantos sao os mesmos
+        else: #sobrou só livre
+            self.figura_nova = Livre(self.cor.get())
+            self.figura_nova.adicionar_ponto(event.x, event.y) #o primeiro ponto é adicionado a lista
+
+    def incluir(self, event): #quando o desenho, termina, verifica se a figura é valida, e, se sim, esta sera adicionada a lista de definitiva
+        if not self.figura_nova.incompleta():
+            self.figuras.append(self.figura_nova)
+
