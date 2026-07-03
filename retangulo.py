e0f05c4c8e571e2507747f971a4b7f09d0ecbabe
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 02:04:48 -0300

    Add Retangulo class for rectangle representation

---
+++ retangulo.py
@@ -0,0 +1,11 @@
+class Retangulo(Figura):
+    def __init__(self, x1, y1, x2, y2, cor, bg):
+        super()._init__(cor, bg)
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+
+    def desenhar(self, canvas):
+        canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, 
+                                outline = self.cor, fill = self.bg)
+
+    def incompleta(self):
+        return (self.x1, self.y1) == (self.x2, self.y2)
