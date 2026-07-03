1b50d39e1d76308f165ec44bc6d027df1f4306f6
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:11:52 -0300

    Add comments to Canvas class methods

--- canvas.py
+++ canvas.py
@@ -1,8 +1,8 @@
-class Canvas:
+class Canvas: #essa classe gere os canavs do tkinter
     def __init__(self, canvas):
-        self.canvas = canvas
+        self.canvas = canvas #recebe o real canvas
     
     def desenhar(self, figuras):
-        self.canvas.delete("all")
+        self.canvas.delete("all") #limpa tudo
         for f in figuras:
-            f.desenhar(self.canvas)
+            f.desenhar(self.canvas) #desenha todas as figuras armazenadas(polimorfismo)
