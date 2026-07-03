239e5f9f7fddbd37d14ca22df75a410c9a62cc6e
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 11:03:36 -0300

    Update circulo.py

--- circulo.py
+++ circulo.py
@@ -1,12 +1,12 @@
 class Circulo(Figura):
-    def __init__(self, x, y, r, cor, bg):
+    def __init__(self, x, y, r, cor, bg): #são armazenados o centro do circulo, bem como seu raio e as cores de preenchimento e 
         super().__init__(cor, bg)
         self.x, self.y, self.r = x, y, r
 
-    def desenhar(self, canvas):
+    def desenhar(self, canvas): #como o tkinter não tem um método para circulo, é utilizado o de ovais com a lógica de calculo de distancia de dois pontos para encontrar o raio
         canvas.create_oval(self.x - self.r, self.y - self.r, 
                            self.x + self.r, self.y + self.r, 
                            outline = self.cor, fill = self.bg)
         
     def incompleta(self):
-        return self.r == 0
+        return self.r == 0 #em aso de figura incompleta, o raio tem comprimento 0
