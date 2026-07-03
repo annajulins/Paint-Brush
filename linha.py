0537d54ae8f8e1a263505b55ca00cd18176fc41f
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 02:03:11 -0300

    Implement Linha class for drawing lines

---
+++ linha.py
@@ -0,0 +1,11 @@
+class Linha(Figura):
+    def __init__(self, x1, y1, x2, y2, cor):
+        super().__init__(cor, "")
+        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+
+    def desenhar(self, canvas):
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, 
+                           fill = self.cor)
+
+    def incompleta(self):
+        return (self.x1, self.y1) == (self.x2, self.y2)
