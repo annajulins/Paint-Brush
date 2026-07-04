1da9bfd356f2fba6ec87f7b78ab1fd1187264eb8
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 23:53:18 -0300

    Remove Canvas_draw class and its methods

--- canvas.py
+++ canvas.py
@@ -1,8 +1,1 @@
-class Canvas_draw: #essa classe gere os canavs do tkinter
-    def __init__(self, canvas):
-        self.canvas = canvas #recebe o real canvas
-    
-    def desenhar(self, figuras):
-        self.canvas.delete("all") #limpa tudo
-        for f in figuras:
-            f.desenhar(self.canvas) #desenha todas as figuras armazenadas(polimorfismo)
+
