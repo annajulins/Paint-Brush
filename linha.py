44a9261318d30b00e8f6fdabd8749cc845285656
becalins <rebeca.souza@dcomp.ufs.br>
2026-07-03 22:11:01 -0300

    Update linha.py

--- linha.py
+++ linha.py
@@ -3,9 +3,9 @@ class Linha(Figura):
         super().__init__(cor, "") #a linha não tem bg, por isso o ""
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #as quatro coordenadas, equivalentes ao ponto inicia e ao final, são armazenadas
 
-    def desenhar(self, canvas): #no caso de arrasto do mouse, a linha é criada a partir do ponto inicial e do ponto final, sendo a sua cor definida por "fill"
+    def desenhar(self, canvas, dash=()): #no caso de arrasto do mouse, a linha é criada a partir do ponto inicial e do ponto final, sendo a sua cor definida por "fill"
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, 
-                           fill = self.cor) 
+                           fill = self.cor, dash="") 
 
     def incompleta(self): #se o mouse não sair do lugar, é retornada uma linha de comprimento 0
         return (self.x1, self.y1) == (self.x2, self.y2)
