d967d458f30195c02ca2c8f85196e89f277ae7a3
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 23:55:07 -0300

    Add dash parameter to desenhar method in oval.py

--- oval.py
+++ oval.py
@@ -3,9 +3,9 @@ class Oval(Figura):
         super().__init__(cor, bg)
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #armazena as coordenadas dos "cantos"
 
-    def desenhar(self, canvas):
+    def desenhar(self, canvas, dash=()):
         canvas.create_oval(self.x1, self.y1, self.x2, self.y2, 
-                           outline = self.cor, fill = self.bg)
+                           outline = self.cor, fill = self.bg, dash=dash)
 
     def incompleta(self):
         return (self.x1, self.y1) == (self.x2, self.y2) #se os dois cantos forem iguais, o oval nao tem tamanho
