692baf26e1438e7a85a25b0c9a267a7f9e9bb315
becalins <rebeca.souza@dcomp.ufs.br>
2026-07-03 22:11:25 -0300

    Update linha.py

--- linha.py
+++ linha.py
@@ -5,7 +5,7 @@ class Linha(Figura):
 
     def desenhar(self, canvas, dash=()): #no caso de arrasto do mouse, a linha é criada a partir do ponto inicial e do ponto final, sendo a sua cor definida por "fill"
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, 
-                           fill = self.cor, dash="") 
+                           fill = self.cor, dash=dash) 
 
     def incompleta(self): #se o mouse não sair do lugar, é retornada uma linha de comprimento 0
         return (self.x1, self.y1) == (self.x2, self.y2)
