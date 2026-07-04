cb4c40140fcf5577d56e9475d7ff5e433a1468c7
annajulins <najumatoslinssantos@gmail.com>
2026-07-03 23:55:56 -0300

    Add dash parameter to desenhar method

--- retangulo.py
+++ retangulo.py
@@ -3,9 +3,9 @@ class Retangulo(Figura):
         super().__init__(cor, bg)
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2 #cantos armazenados por meio de dois pares de coordenadas
 
-    def desenhar(self, canvas):
+    def desenhar(self, canvas, dash=()):
         canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, 
-                                outline = self.cor, fill = self.bg)
+                                outline = self.cor, fill = self.bg, dash=dash)
 
     def incompleta(self):
         return (self.x1, self.y1) == (self.x2, self.y2) #raciocinio parece com o de oval, ja que, na igualdade dos cantos, o retangulo nao tem tamanho
