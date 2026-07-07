1adaf04000084b889cdaea7a4e894ce0bf6b80f5
becalins <rebeca.souza@dcomp.ufs.br>
2026-07-07 10:44:24 -0300

    corrigindo identação pq estava errada tbm e podia dar erro

--- janelaPaint.py
+++ janelaPaint.py
@@ -6,33 +6,33 @@ class JanelaPaint:
     self.frame = Frame(self.root)
     self.frame.pack()
 
-  #Label
-  Label(self.frame,
-         text = 'Linha, Livre, Retangulo, Circulo'
-          .grid(column=1, row=0, padx=5, pady=5)
-  
-  #Area de desenho
-  self.canvas = Canvas(self.frame,
-                      bg= 'white',
-                      width= 700,
-                      height= 500)
-  
-  self.canvas.grid(column=1, row=0, columnspan= 2)
-  
-  #Menu
-  OptionMenu(self.frame, self.tipo,
-            'Linha', 'Livre', 'Oval', 'Retangulo', 'Circulo'
-             ).grid(column= 1, row= 0)
+    #Label
+    Label(self.frame,
+           text = 'Linha, Livre, Retangulo, Circulo'
+            .grid(column=1, row=0, padx=5, pady=5)
+    
+    #Area de desenho
+    self.canvas = Canvas(self.frame,
+                        bg= 'white',
+                        width= 700,
+                        height= 500)
+    
+    self.canvas.grid(column=1, row=0, columnspan= 2)
+    
+    #Menu
+    OptionMenu(self.frame, self.tipo,
+              'Linha', 'Livre', 'Oval', 'Retangulo', 'Circulo'
+               ).grid(column= 1, row= 0)
 
-  OptionMenu(
-    self.root
-    self.cor
-    self.cores
-  ).pack()
-  
-  
-  OptionMenu(
-    self.root
-    self.cor
-    self.cores
-  ).pack()
+    OptionMenu(
+      self.root
+      self.cor
+      self.cores
+    ).pack()
+    
+    
+    OptionMenu(
+      self.root
+      self.cor
+      self.cores
+    ).pack()
