467228f1ee5a9031f55c0435f5fa57b1670387b6
becalins <rebeca.souza@dcomp.ufs.br>
2026-07-07 22:39:09 -0300

    main atualizada com todos os arquivos importados

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-window = Tk()
-app = Geral(window)
-window.mainloop()
+from modelo.figuras import *
+from janelaPaint import *
+from modelo.desenho import *
+from controladorPaint import *
+
+visao = JanelaPaint()
+desenho = Desenho(visao)
+controlador = ControladorPaint(desenho, visao)
+visao.root.mainloop()
