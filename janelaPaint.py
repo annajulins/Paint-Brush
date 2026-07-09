81c4009e3f4e957dc6f673db15feea8da9c7f09d
Anna Julia Matos Lins Santos <najumatoslinssantos@gmail.com>
2026-07-09 01:29:31 -0300

    explicando partes do código
    Added comments for clarity and organization.

--- janelaPaint.py
+++ janelaPaint.py
@@ -1,25 +1,26 @@
 from tkinter import *
 from tkinter import ttk
 
-
 class JanelaPaint:
-  def __init__(self):
+  def __init__(self): #abrir a janela tkinter
     self.root = Tk()
     self.frame = Frame(self.root)
     self.frame.pack()
 
+    #vetor das cores a serem escolhidas
     self.cores = ["black", "red", "orange", "yellow", "green", "blue", "purple", "white"]
 
+    #definindo a cor de desenho, o tipo de imagem/figura a ser desenhado e a sua cor de preenchimento
     self.tipo = StringVar(value='Linha')
     self.cor = StringVar(value = self.cores[0])
     self.bg = StringVar(value = self.cores[0])
 
-    #Label
+    #Label, o que vai aparecer como "título" do programa
     Label(self.frame,
          text = 'Linha, Livre, Retangulo, Oval ou Circulo'
           ).grid(column=1, row=0, padx=5, pady=5)
   
-    #Menu
+    #Menu, as opções de coisas a serem desenhadas
     OptionMenu(self.frame, self.tipo,
             'Linha', 'Livre', 'Oval', 'Retangulo', 'Circulo'
              ).grid(column= 1, row= 1)
