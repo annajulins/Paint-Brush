a9fdb79ffb504cee50d4fbc7838fa78ee5102fad
Anna Julia Matos Lins Santos <najumatoslinssantos@gmail.com>
2026-07-10 11:06:23 -0300

    comentando a janelaPaint.py
    Updated comments for clarity and detail in janelaPaint.py.

--- janelaPaint.py
+++ janelaPaint.py
@@ -4,23 +4,23 @@ from tkinter import ttk
 class JanelaPaint:
   def __init__(self): #abrir a janela tkinter
     self.root = Tk()
-    self.frame = Frame(self.root)
-    self.frame.pack()
+    self.frame = Frame(self.root) #o frame é tipo o recipiente para organizar widgets
+    self.frame.pack() #o pack coloca o frame dentro da janela
 
     #vetor das cores a serem escolhidas
     self.cores = ["black", "red", "orange", "yellow", "green", "blue", "purple", "white"]
 
-    #definindo a cor de desenho, o tipo de imagem/figura a ser desenhado e a sua cor de preenchimento
+    #definindo (e armazenando) a cor de desenho, o tipo de imagem/figura a ser desenhado e a sua cor de preenchimento
     self.tipo = StringVar(value='Linha')
     self.cor = StringVar(value = self.cores[0])
     self.bg = StringVar(value = self.cores[0])
 
-    #Label, o que vai aparecer como "título" do programa
+    #Label, o que vai aparecer como "título" do programa (título da interface)
     Label(self.frame,
          text = 'Linha, Livre, Retangulo, Oval ou Circulo'
-          ).grid(column=1, row=0, padx=5, pady=5)
+          ).grid(column=1, row=0, padx=5, pady=5) #grid() posiciona o widget numa tabela, e os pads são os espaçamentos
   
-    #Menu, as opções de coisas a serem desenhadas
+    #Menu, as opções a serem desenhadas
     OptionMenu(self.frame, self.tipo,
             'Linha', 'Livre', 'Oval', 'Retangulo', 'Circulo'
              ).grid(column= 1, row= 1)
@@ -36,7 +36,7 @@ class JanelaPaint:
           self.bg,
           *self.cores
         ).pack()
-    
+    #o * desempacota a lista de cores
     #Area de desenho
     self.canvas = Canvas(self.frame,
                       bg= 'white',
