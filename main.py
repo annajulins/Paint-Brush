834c86f175ad76dd042ac0f721e0c74333806963
Anna Julia Matos Lins Santos <najumatoslinssantos@gmail.com>
2026-07-13 15:56:34 -0300

    alterações na main relacionadas à alteração dos arquivos
    Added Arquivos and ferramentaPaint imports and initialized Arquivos instance.

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from modelo.figuras import *
 from janelaPaint import *
 from modelo.desenho import *
 from controladorPaint import *
+from arquivosPaint import *
+from ferramentaPaint import *
 
 visao = JanelaPaint()
 desenho = Desenho(visao)
 controlador = ControladorPaint(desenho, visao)
+arquivos = Arquivos(desenho, visao)
+
+
+visao.arquivos = arquivos
 visao.root.mainloop()
